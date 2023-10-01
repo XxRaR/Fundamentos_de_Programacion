cf6c66ccd703bde916897e91bb23bf399f1c9edf
XxRaR <146400597+XxRaR@users.noreply.github.com>
2023-10-01 17:45:27 -0600

    Add files via upload

---
+++ P_10/P_10/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 10. Realizar un algoritmo que imprima el nombre */
+using System;
+
+namespace P_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string nombre, apellidos, completo;
+            Console.Write("Ingresa tus nombres ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingresa tus apellidos");
+            apellidos = Console.ReadLine();
+            completo = nombre + apellidos;
+
+            Console.WriteLine(" * * Bienvenido * * " + completo + " * * ");
+            Console.WriteLine(" -- Gracias :)");
+
+        }
+    }
+}
