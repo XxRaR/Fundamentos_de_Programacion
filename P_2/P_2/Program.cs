16af001365184266e89ea79965d81b297c4ebf0f
XxRaR <146400597+XxRaR@users.noreply.github.com>
2023-10-01 17:34:08 -0600

    Add files via upload

---
+++ P_2/P_2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 2.Realizar un algoritmo que permita calcular la 
+siguiente formula: x=y+a+3 */
+using System;
+namespace P_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, a;
+            Console.Write("Escribe el valor para y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Escribe el valor para a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            //Se calcula la formula x=y+a+3
+            x = y + a + 3;
+            Console.Write($"El valor de x es: {x}");
+        }
+    }
+}
---
+++ P_3/P_3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 3.Realizar un algoritmo que calcule el area y 
+perimetro de un triangulo */
+using System;
+namespace P_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal b, h, p, A, l;
+            Console.Write("Ingresa la altura del triangulo: ");
+            h = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Ingresa la base del triangulo: ");
+            b = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Ingresa el lado del triangulo: ");
+            l = Convert.ToDecimal(Console.ReadLine());
+            A = (b * h) / 2;
+            p = l + l + l;
+            Console.WriteLine("El area del triangulo es: " + A);
+
+            Console.Write($"El perimetro del triangulo es: " + p);
+        }
+    }
+}
