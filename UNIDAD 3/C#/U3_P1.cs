0b260335485ebd8e5494794d4a227cf61fa1873c
XxRaR <146400597+XxRaR@users.noreply.github.com>
2023-10-22 14:26:47 -0600

    Add files via upload

---
+++ UNIDAD 3/C#/U3_P1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace U3_P1
+{
+    internal class U3_P1
+    {
+        static void Main(string[] args)
+        {
+
+            int edad; //2
+            Console.Write("Escribe tu edad: "); //3
+            edad = Convert.ToInt32(Console.ReadLine()); //4
+            if (edad >= 18) //5 
+
+                Console.WriteLine("Eres mayor de edad!"); //5
+        else //6
+                Console.WriteLine("Eres menor de edad!"); //6
+        }
+    }
+}
