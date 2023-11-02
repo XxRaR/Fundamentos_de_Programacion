183766850bc25f7bc4359c2eb669f0eaea23ae80
XxRaR <146400597+XxRaR@users.noreply.github.com>
2023-11-01 19:40:59 -0600

    Update U3_P18.cs

--- UNIDAD 3/C#/U3_P18.cs
+++ UNIDAD 3/C#/U3_P18.cs
@@ -1,20 +1,20 @@
-﻿using System;
+//Lo realice como si fuera el salario anual
+using System;
 namespace U3_P18
 {
     internal class U3_P18
     {
         static void Main(string[] args)
         {
-            double S = 1500, I = 0.10, i;
+            double S = 1500, I = 0.10, i, s = 0;
             int años = 6;
             for (i = 1; i <= años && años > 0; i++)
             {
                 S = (S * I) + S;
-                Console.WriteLine("El salario anual" + i + " es de: $" + S);
-
+                Console.WriteLine("El salario anual " + i + " es de: $" + S);
+                s = s + S;
             }
-            Console.WriteLine("El salario a los 6 años es: " + S);
-
+            Console.WriteLine("El salario a los 6 años es: $" + s);
         }
     }
 }
