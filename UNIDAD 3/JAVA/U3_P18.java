40f663c50a032d080e18e2b64c8f13046c8a94bb
XxRaR <146400597+XxRaR@users.noreply.github.com>
2023-11-01 19:30:39 -0600

    Update U3_P18.java

--- UNIDAD 3/JAVA/U3_P18.java
+++ UNIDAD 3/JAVA/U3_P18.java
@@ -11,14 +11,14 @@ package com.mycompany.u3_p18;
 public class U3_P18 {
 
     public static void main(String[] args) {
-        double S= 1500, I=0.10,i;
+        double S= 1500, I=0.10,i, s = 0;
         int años=6;
         for (i=1; i<=años && años>0; i++){
         S = (S*I) + S;
         System.out.println("El salario anual" + i + " es de: $" + S);        
-            
+        s=s+S;    
     }
-        System.out.println("El salario a los 6 años es: " + S);
+        System.out.println("El salario a los 6 años es: $" + s);
          
 
          
